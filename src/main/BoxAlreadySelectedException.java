8d3a8f3e61b3538cb6d96253b9e310b9592e70bc
nnouzzinsa <nnouzzinsa@nespresso.com>
2015-01-19 12:34:58 +0100

    init repo

---
+++ src/main/BoxAlreadySelectedException.java
@@ -0,0 +1,4 @@
+
+public class BoxAlreadySelectedException extends Exception{
+
+}
---
+++ src/main/Morpion.java
@@ -0,0 +1,27 @@
+import org.hamcrest.Matcher;
+
+
+public class Morpion {
+
+	public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+	
+	public Morpion(String string, String string2, String string3) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void play(String string, String string2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Matcher<String> report() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Matcher<String> display() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
